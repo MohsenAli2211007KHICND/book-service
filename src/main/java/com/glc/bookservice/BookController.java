b90e0effa6d371ff16ea33de58af6d473100df78
MohsenAli2211007KHICND <mohsen.ali@xloopdigital.com>
2023-01-05 15:47:28 +0500

    Completed

--- src/main/java/com/glc/bookservice/BookController.java
+++ src/main/java/com/glc/bookservice/BookController.java
@@ -1,7 +1,14 @@
 package com.glc.bookservice;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,10 +18,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @RestController
 @RequestMapping("/books")  // Any address like https://localhost:8080/books
+@RabbitListener(queues = "book-list")
 public class BookController {
     private final BookRepository repository;
+
+    @Autowired
+    private Queue queue;
+    @Autowired
+    private RabbitTemplate temp;
 
     public BookController(BookRepository repository){
         this.repository = repository;
@@ -41,6 +57,25 @@ public class BookController {
     @PutMapping("")
     public Book updateSpecificBook(@RequestBody Book myBook){
         return this.repository.updateSpecificBook(myBook);
+    }
+
+    @RabbitHandler
+    public void receiver (String msg) throws Exception {
+        ObjectMapper objMap = new ObjectMapper();
+        List<Book> myBook = objMap.readValue(msg, new TypeReference<List<Book>>(){});
+        myBook.forEach((book) -> repository.save(book));
+        myBook.forEach((book) -> System.out.println(book.getTitle()));
+    }
+
+    @PostMapping("/booklist") // http://localhost:8080/books/booklist
+    public void sender() throws Exception{
+        ObjectMapper objMap = new ObjectMapper();
+        Book book1 = new Book(3,"Code With Mosh","Mosh", 1998, 2220);
+        Book book2 = new Book(4,"Code With Jeff","Dr. Jeff", 1978, 210);
+        List<Book> myBooks = new ArrayList<>();
+        myBooks.add(book1);
+        myBooks.add(book2);
+        temp.convertAndSend(queue.getName(), objMap.writeValueAsString(myBooks));
     }
 
 
--- src/main/java/com/glc/bookservice/BookServiceApplication.java
+++ src/main/java/com/glc/bookservice/BookServiceApplication.java
@@ -1,10 +1,17 @@
 package com.glc.bookservice;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookServiceApplication {
+
+	@Bean
+	Queue queue (){
+		return new Queue("book-list");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookServiceApplication.class, args);
